1a4c2a2385a3370273ffaa8f1881262838e4c654
ZinurX <145262061+ZinurX@users.noreply.github.com>
2024-12-12 13:04:17 +0300

    send message

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +12,10 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
+		return args -> {
+			kafkaTemplate.send("amigo","one");
+		};
+	}
 }
